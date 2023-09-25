857296d1264dca59e03a80172d5528a35bb47f77
razdobudko.s <razdobudko.s@wb.ru>
2023-09-25 18:44:00 +0200

    Test Addition of Shopping Items

--- testing-on-android/src/androidTest/java/com/mcmouse88/testing_on_android/presentation/AddShoppingItemFragmentTest.kt
+++ testing-on-android/src/androidTest/java/com/mcmouse88/testing_on_android/presentation/AddShoppingItemFragmentTest.kt
@@ -1,12 +1,18 @@
 package com.mcmouse88.testing_on_android.presentation
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth
+import com.mcmouse88.testing_on_android.R
+import com.mcmouse88.testing_on_android.data.local.ShoppingItem
+import com.mcmouse88.testing_on_android.getOrAwaitValue
 import com.mcmouse88.testing_on_android.launchFragmentInHiltContainer
+import com.mcmouse88.testing_on_android.repository.FakeShoppingRepositoryAndroidTest
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
@@ -14,7 +20,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
-import com.mcmouse88.testing_on_android.R
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -22,6 +28,12 @@ class AddShoppingItemFragmentTest {
 
     @get:Rule
     val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    val taskExecutor = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
 
     @Before
     fun setup() {
@@ -31,7 +43,9 @@ class AddShoppingItemFragmentTest {
     @Test
     fun pressBackButton_navigateUp() {
         val navController = mock(NavController::class.java)
-        launchFragmentInHiltContainer<AddShoppingItemFragment> {
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
             Navigation.setViewNavController(requireView(), navController)
         }
 
@@ -43,7 +57,9 @@ class AddShoppingItemFragmentTest {
     @Test
     fun clickShoppingItem_navigateToToImagePickFragment() {
         val navController = mock(NavController::class.java)
-        launchFragmentInHiltContainer<AddShoppingItemFragment> {
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
             Navigation.setViewNavController(requireView(), navController)
         }
 
@@ -52,5 +68,37 @@ class AddShoppingItemFragmentTest {
         Mockito.verify(navController).navigate(
             AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
         )
+    }
+
+    @Test
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDb() {
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+            viewModel = testViewModel
+        }
+
+        Espresso.onView(ViewMatchers.withId(R.id.et_shopping_item_name))
+            .perform(ViewActions.replaceText("shopping item"))
+
+        Espresso.onView(ViewMatchers.withId(R.id.et_shopping_item_amount))
+            .perform(ViewActions.replaceText("5"))
+
+        Espresso.onView(ViewMatchers.withId(R.id.et_shopping_item_price))
+            .perform(ViewActions.replaceText("5.5"))
+
+        Espresso.onView(ViewMatchers.withId(R.id.btn_add_shopping_item))
+            .perform(ViewActions.click())
+
+        Truth.assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(
+                ShoppingItem(
+                    name = "shopping item",
+                    amount = 5,
+                    price = 5.5f,
+                    imageUrl = ""
+                )
+            )
     }
 }
--- testing-on-android/src/main/java/com/mcmouse88/testing_on_android/presentation/AddShoppingItemFragment.kt
+++ testing-on-android/src/main/java/com/mcmouse88/testing_on_android/presentation/AddShoppingItemFragment.kt
@@ -4,24 +4,39 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
 import com.mcmouse88.testing_on_android.R
 import com.mcmouse88.testing_on_android.databinding.FragmentAddShoppingItemBinding
-import dagger.hilt.android.AndroidEntryPoint
+import com.mcmouse88.testing_on_android.others.Status
+import javax.inject.Inject
 
-@AndroidEntryPoint
-class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
+class AddShoppingItemFragment @Inject constructor(
+    val glide: RequestManager
+) : Fragment(R.layout.fragment_add_shopping_item) {
 
     private var _binding: FragmentAddShoppingItemBinding? = null
     private val binding: FragmentAddShoppingItemBinding
         get() = _binding ?: throw IllegalStateException("FragmentAddShoppingItemBinding is null")
 
-    private val viewModel by activityViewModels<ShoppingViewModel>()
+    lateinit var viewModel: ShoppingViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentAddShoppingItemBinding.bind(view)
+        viewModel = ViewModelProvider(requireActivity())[ShoppingViewModel::class.java]
+
+        subscribeToObservers()
+
+        binding.btnAddShoppingItem.setOnClickListener {
+            viewModel.insertShoppingItem(
+                name = binding.etShoppingItemName.text.toString(),
+                amountString = binding.etShoppingItemAmount.text.toString(),
+                priceString = binding.etShoppingItemPrice.text.toString()
+            )
+        }
 
         binding.ivShoppingItem.setOnClickListener {
             findNavController().navigate(
@@ -38,5 +53,34 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.currentImageUrl.observe(viewLifecycleOwner) {
+            glide.load(it).into(binding.ivShoppingItem)
+        }
+
+        viewModel.insertShoppingItemStatus.observe(viewLifecycleOwner) {
+            it.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        Snackbar.make(
+                            binding.root,
+                            "Added shopping item",
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                        findNavController().navigateUp()
+                    }
+                    Status.ERROR -> {
+                        Snackbar.make(
+                            binding.root,
+                            result.message ?: "Unknown error occurred",
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                    }
+                    Status.LOADING -> { /* no-op */ }
+                }
+            }
+        }
     }
 }
