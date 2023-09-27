017cc110f8a280f0d59a092d163fb9a5ee91f0d0
razdobudko.s <razdobudko.s@wb.ru>
2023-09-28 00:08:19 +0200

    One-to-N Relationships

--- multiple-table-database/src/main/java/com/mcmouse88/multiple_table_database/dao/SchoolDao.kt
+++ multiple-table-database/src/main/java/com/mcmouse88/multiple_table_database/dao/SchoolDao.kt
@@ -7,7 +7,9 @@ import androidx.room.Query
 import androidx.room.Transaction
 import com.mcmouse88.multiple_table_database.entities.Director
 import com.mcmouse88.multiple_table_database.entities.School
+import com.mcmouse88.multiple_table_database.entities.Student
 import com.mcmouse88.multiple_table_database.entities.relations.SchoolAndDirectorRelation
+import com.mcmouse88.multiple_table_database.entities.relations.SchoolWithStudents
 
 @Dao
 interface SchoolDao {
@@ -21,4 +23,11 @@ interface SchoolDao {
     @Transaction
     @Query("SELECT * FROM school_table WHERE school_name = :schoolName")
     suspend fun getSchoolAndDirectorWithSchoolName(schoolName: String): List<SchoolAndDirectorRelation>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStudent(student: Student)
+
+    @Transaction
+    @Query("SELECT * FROM school_table WHERE school_name = :schoolName")
+    suspend fun getSchoolWithStudents(schoolName: String): List<SchoolWithStudents>
 }
--- multiple-table-database/src/main/java/com/mcmouse88/multiple_table_database/entities/relations/SchoolWithStudents.kt
+++ multiple-table-database/src/main/java/com/mcmouse88/multiple_table_database/entities/relations/SchoolWithStudents.kt
@@ -3,16 +3,16 @@ package com.mcmouse88.multiple_table_database.entities.relations
 import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Relation
-import com.mcmouse88.multiple_table_database.entities.Director
 import com.mcmouse88.multiple_table_database.entities.School
+import com.mcmouse88.multiple_table_database.entities.Student
 
-data class SchoolAndDirectorRelation(
+data class SchoolWithStudents(
     @[Embedded ColumnInfo("school")]
     val school: School,
     @Relation(
         parentColumn = "school_name",
         entityColumn = "school_name"
     )
-    @ColumnInfo("director")
-    val director: Director
+    @ColumnInfo("students")
+    val students: List<Student>
 )
