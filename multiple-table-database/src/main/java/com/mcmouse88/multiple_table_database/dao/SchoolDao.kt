469e8aca87a9c5069c07babc97ded1c50c5e29fc
razdobudko.s <razdobudko.s@wb.ru>
2023-09-27 23:55:36 +0200

    One-to-One Relationships with Room

---
+++ multiple-table-database/src/main/java/com/mcmouse88/multiple_table_database/dao/SchoolDao.kt
@@ -0,0 +1,24 @@
+package com.mcmouse88.multiple_table_database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.mcmouse88.multiple_table_database.entities.Director
+import com.mcmouse88.multiple_table_database.entities.School
+import com.mcmouse88.multiple_table_database.entities.relations.SchoolAndDirectorRelation
+
+@Dao
+interface SchoolDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSchool(school: School)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertDirector(director: Director)
+
+    @Transaction
+    @Query("SELECT * FROM school_table WHERE school_name = :schoolName")
+    suspend fun getSchoolAndDirectorWithSchoolName(schoolName: String): List<SchoolAndDirectorRelation>
+}
