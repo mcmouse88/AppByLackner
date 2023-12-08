7b32afe906beaea97106bd5eb56399dfe79f8bb7
razdobudko.s <razdobudko.s@wb.ru>
2023-12-08 01:40:27 +0100

    Refactoring modules

--- navigation/navigate-back-result/src/main/java/com/mcmouse88/navigate_back_result/MainActivity.kt
+++ navigation/navigate-back-result/src/main/java/com/mcmouse88/navigate_back_result/MainActivity.kt
@@ -7,13 +7,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -22,7 +21,7 @@ import androidx.navigation.compose.rememberNavController
 import com.mcmouse88.navigate_back_result.ui.theme.NavigateBackWithResultTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
