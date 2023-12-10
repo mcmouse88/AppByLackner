1c383e403904a75108cd149054a2402be50c831d
razdobudko.s <razdobudko.s@wb.ru>
2023-12-10 15:45:42 +0100

    How to EASILY Implement a Lazy Staggered Grid In Jetpack Compose

---
+++ compose-lists/lazy-staggered-grid/src/main/java/com/mcmouse88/lazy_staggered_grid/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.mcmouse88.lazy_staggered_grid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.mcmouse88.lazy_staggered_grid.ui.theme.LazyStaggeredGridTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val items = (1..100).map {
+            ListItem(
+                height = Random.nextInt(100, 300).dp,
+                color = Color(
+                    Random.nextLong(0xFFFFFFFF)
+                ).copy(alpha = 1f)
+            )
+        }
+        setContent {
+            LazyStaggeredGridTheme {
+                LazyVerticalStaggeredGrid(
+                    columns = StaggeredGridCells.Fixed(2),
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalItemSpacing = 16.dp
+                ) {
+                    items(items) {item ->
+                        RandomColorBox(item = item)
+                    }
+                }
+            }
+        }
+    }
+}
+
+data class ListItem(
+    val height: Dp,
+    val color: Color
+)
+
+@Composable
+fun RandomColorBox(
+    item: ListItem,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(item.height)
+            .clip(RoundedCornerShape(12.dp))
+            .background(item.color)
+    )
+}
